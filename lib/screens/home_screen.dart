fe59ccb7bf835e1af85b34a928303902d06a2e6c
FlutLab.io <>
2022-05-11 22:18:55 +0000

    Autosave

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -4,33 +4,25 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
         appBar: AppBar(
           title: Text("Flutter AppBar Example"),
           centerTitle: true,
         ),
         body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Conenedor")
-        ],
-      )
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Contador")
+          ],
+        )),
+        floatingActionButton: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
           FloatingActionButton(
-            onPressed: () {
-
-            },
-            child: Icon
-            (Icons.add, 
-            size: 30,
-            )
-          ),
-        ]
-       )
-     );
+              onPressed: () {},
+              child: Icon(
+                Icons.add,
+                size: 30,
+              )),
+        ]));
   }
 }
