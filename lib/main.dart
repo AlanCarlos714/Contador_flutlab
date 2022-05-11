ea3fe69c55e359958fcd5df55942b50c5a22458c
FlutLab.io <>
2022-05-11 22:24:11 +0000

    Autosave

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: HomeScreen(),
         theme: ThemeData(
-            primarySwatch: Colors.grey,
+            primarySwatch: Colors.blueGrey,
             accentColor: Colors.pink,
             textTheme: TextTheme(
                 bodyText2: TextStyle(
