50cd24ba6cd9b0cb0bf704186d39f4b2fcdfb621
FlutLab.io <>
2022-05-11 22:27:14 +0000

    Autosave

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -24,6 +24,7 @@ class HomeScreen extends StatelessWidget {
           FloatingActionButton(
               onPressed: () {
                contador++;
+               print("Hola soy un contador: $contador");
               },
               child: Icon(
                 Icons.add,
